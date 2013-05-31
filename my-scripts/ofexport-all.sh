dfb7b18a2f628fc763a46ea22a5c9b48c1ce6491
infotexture <infotexture@gmail.com>
2013-05-31 13:35:58 +0200

    Rename script & set up ofexport environment for use w/ launchd or Hazel

---
+++ my-scripts/ofexport-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run this script (via Hazel or Launchd) whenever the OmniFocus database is updated
+
+# Since `source ~/.bash_profile` doesn't work in Hazel or launchd, set PATH here
+
+# 2013-05-17: Explicitly add OmniFocus `ofexport` tool to PATH
+# See <https://github.com/psidnell/ofexport/blob/master/DOCUMENTATION.md>
+export OFEXPORT_HOME="/Users/rofish/Documents/Setup/Scripts/OmniFocus/ofexport"
+export PATH=$PATH:"$OFEXPORT_HOME/bin"
+
+# Export the full content with all notes, tags, contexts, backlink URLs, etc.
+ofexport -o ~/Documents/Dropbox/Notes/OmniFocus-truth.todo
+
+# Export a simpler, less cluttered version with just the projects and tasks
+ofexport -T taskpaper-lite -o ~/Documents/Dropbox/Notes/OmniFocus-tasks.todo
+
+# Exit with status of last command.
+exit $?
